9844c17e24158fde75702b02cc46dba98e3f31ee
Jacob Greer <jacobmgreer@users.noreply.github.com>
2023-09-30 17:03:54 -0400

    fix

--- Geographies/AMPAS-1994/mouseovermap.js
+++ Geographies/AMPAS-1994/mouseovermap.js
@@ -31,7 +31,7 @@ function mapInit(data) {
     let mousemove = function(event) {
         tooltip
             .style("left", (event.pageX + 20) + 'px')
-            style("top", (event.pageY - 20) + 'px');
+            .style("top", (event.pageY - 20) + 'px');
     }
     let mouseout = function() {
         d3.select(this)
